9954f3b812cbfeaa953cb2ee1b064ad5a66ac9f1
Sajid Ali <43712400+sajidalighazi@users.noreply.github.com>
2023-05-08 22:45:49 +0200

    Add files via upload

---
+++ Python Program to Add Two Numbers.py
@@ -0,0 +1,11 @@
+num1=10
+num2=20
+sum=num1+num2
+print("The sum of two given number is ", sum)
+
+#Solution 2 from user input
+
+num1=float(input("Enter the Ist number hare "))
+num2=float(input("Enter the second number hare "))
+sum=num1+num2
+print("The Sum of you number is ", sum)
